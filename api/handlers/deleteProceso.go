0438f4de36e7665706438c42f6b9e967c249d539
GabrieldlC <81653569+GabrieldlC@users.noreply.github.com>
2024-06-19 16:11:13 -0300

    delete congelados

--- api/handlers/deleteProceso.go
+++ api/handlers/deleteProceso.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"Nueva/modelos"
 	"database/sql"
 	"encoding/json"
 	"fmt"
@@ -15,6 +16,33 @@ func DeleteProceso(db *sql.DB) http.HandlerFunc {
 
 		idProceso := vars["idProceso"]
 
+		// Obtengo el proceso a borrar
+		querySelect := "select id_modelo, fecha_desde, fecha_hasta, num_version from extractor.ext_procesados where id_proceso = $1"
+		var proceso modelos.DTOproceso
+
+		err := db.QueryRow(querySelect, idProceso).Scan(&proceso.Id_modelo, &proceso.Fecha_desde, &proceso.Fecha_hasta, &proceso.Version)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, "Proceso no encontrado", http.StatusNotFound)
+			} else {
+				fmt.Println("Error al ejecutar query: ", err.Error())
+				http.Error(w, "Error en el servidor: "+err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		// Borro registro de control congelado
+		if proceso.Fecha_desde == proceso.Fecha_hasta {
+			queryDeleteCongelado := "delete from extractor.ext_control_congelado where id_modelo = $1 and fecha = $2 and num_version = $3"
+			_, err = db.Exec(queryDeleteCongelado, proceso.Id_modelo, proceso.Fecha_desde, proceso.Version)
+			if err != nil {
+				fmt.Println("Error al eliminar de otra tabla: ", err.Error())
+				http.Error(w, "Error en el servidor: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		// Finalmente borro registro de proceso
 		query := "delete from extractor.ext_procesados where id_proceso = $1"
 
 		result, err := db.Exec(query, idProceso)
